808ac8167bd7eaf91ef09b02f795370505f488d3
gabberthomson <gabbriellini@gmail.com>
2014-03-09 21:36:13 +0100

    devel

--- atschool/models.py
+++ atschool/models.py
@@ -2,13 +2,34 @@ from django.db import models
 
 # Create your models here.
 
-class School(models.Model):
-    descr = models.CharField(max_length=100)
+class AnnoScolastico(models.Model):
+    anni = models.CharField(max_length=10)
 
-class Class(models.Model):
-    descr = models.CharField(max_length=100)
-    school = models.ForeignKey(School)
+class Scuola(models.Model):
+    nome = models.CharField(max_length=100)
 
-class Subject(models.Model):
-    descr = models.CharField(max_length=100)
+class Classe(models.Model):
+    nome = models.CharField(max_length=100)
+    scuola = models.ForeignKey(Scuola)
+
+class Materia(models.Model):
+    nome = models.CharField(max_length=100)
+    
+class Studente(models.Model):
+    nome = models.CharField(max_length=100)
+    cognome = models.CharField(max_length=100)
+    data_nascita = models.DateField()
+    
+    class Meta:
+        unique_together = ("nome", "cognome", "data_nascita") 
+
+class DettaglioStudente(models.Model):
+    studente = models.ForeignKey(Studente)
+    annoscolastico = models.ForeignKey(AnnoScolastico)
+    classe = models.ForeignKey(Classe)
+    scuola = models.ForeignKey(Scuola)
+
+    class Meta:
+        unique_together = ("studente", "annoscolastico", "classe", "scuola") 
+
     
